33969b4ca9009257deb6e4a5c0e722d7e681d834
Shivam <info@shivamjha.com>
2026-03-19 16:34:50 +0530

    nodejs backend removed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
-import { viteExpressPlugin } from './backend/vite-plugin.ts'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: { port: 6712 },
-  plugins: [viteExpressPlugin(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
